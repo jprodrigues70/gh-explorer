906b933b16e7296d1df0b451b5850ee79f928f37
caiza fortunato <caizafortunato27@gmail.com>
2021-05-12 20:21:21 -0300

    finalizacao da conexao de banco

--- gh-explorer/src/ConexaoBanco/TesteConexao.java
+++ gh-explorer/src/ConexaoBanco/TesteConexao.java
@@ -1,33 +1,21 @@
 package ConexaoBanco;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+
+import GitHubEntity.Repo;
+import GitHubEntity.User;
 
 public class TesteConexao {
 	java.sql.Statement stm = null;
-	String sqlRepo = "insert into Repo (name, owner_name, url, stars, forks)" +" values (?,?,?,?,?,?)";
-	String sqlUser = "insert into User (name, login, url, location, email)" + " values (?,?,?,?,?)";
 
-	public void TestaConexao() throws ClassNotFoundException, SQLException {
-		stm = Conexao.getInstacia().getConector().createStatement();
-		Connection c = Conexao.getInstacia().getConector();
-	//	insertRepo();
-		stm.executeUpdate(sqlRepo);
-		stm.executeUpdate(sqlUser);
-		
-		//stmt.execute();
-        //stmt.close();
-	}
 	
-//	insertRepo(){
-//		
-//		PreparedStatement stmt = c.prepareStatement(sqlRepo);
-//		stmt.setString(1, x);
-//		stmt.setString(2, x);
-//		stmt.setString(3, x);
-//		stmt.setString(4, x);
-//		stmt.setString(5, x);
-//		stmt.setString(6, x);
-//	}
+
+	public void TestaConexao(Repo repo, User user) throws ClassNotFoundException, SQLException {
+		stm = Conexao.getInstacia().getConector().createStatement();
+
+		stm.executeUpdate("INSERT INTO Repo (name, owner_name, url, stars, forks) values ("+", "+repo.getName()+", "+ repo.getOwner_name()+", "+ repo.getUrl()+", "+ repo.getStars()+", "+ repo.getForks());
+		stm.executeUpdate("INSERT INTO User (name, login, url, location, email)" + " values ("+", "+user.getName()+ ", " + user.getLogin() +", " + 	user.getUrl() +", "+ user.getLocation() +", " +user.getEmail());
+	
+	}
+
 }
--- gh-explorer/src/GitHubEntity/Repo.java
+++ gh-explorer/src/GitHubEntity/Repo.java
@@ -7,4 +7,42 @@ public class Repo {
 	private String url;
 	private String stars;
 	private String forks;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getOwner_name() {
+		return owner_name;
+	}
+	public void setOwner_name(String owner_name) {
+		this.owner_name = owner_name;
+	}
+	public String getOwner_id() {
+		return owner_id;
+	}
+	public void setOwner_id(String owner_id) {
+		this.owner_id = owner_id;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getStars() {
+		return stars;
+	}
+	public void setStars(String stars) {
+		this.stars = stars;
+	}
+	public String getForks() {
+		return forks;
+	}
+	public void setForks(String forks) {
+		this.forks = forks;
+	}
+	
+	
 }
